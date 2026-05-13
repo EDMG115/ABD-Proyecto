f08553d62bc07450ed43fbc81481a937a733568d
CarlosCVG <L23550322@chihuahua2.tecnm.mx>
2026-05-13 08:39:28 -0600

    revisado

--- public/src/data/DAO/backupAdminDAO.php
+++ public/src/data/DAO/backupAdminDAO.php
@@ -4,7 +4,7 @@ error_reporting(E_ALL);
 ini_set('display_errors', 1);
 require_once "./../conexion.php";
 
-    $conexion = new Conexion();
+$conexion = new Conexion();
 
 if (
     isset($_POST['accion']) &&
@@ -12,14 +12,14 @@ if (
 ) {
 
     $carpeta =
-    __DIR__ . "/../DB/backups/";
+        __DIR__ . "/../DB/backups/";
 
     $archivos =
-    glob($carpeta . "*.sql");
+        glob($carpeta . "*.sql");
 
-    usort($archivos, function($a, $b) {
-    return filemtime($b) - filemtime($a);
-});
+    usort($archivos, function ($a, $b) {
+        return filemtime($b) - filemtime($a);
+    });
     $resultado = [];
 
     foreach ($archivos as $archivo) {
@@ -51,63 +51,61 @@ try {
     $cred = $conexion->getCredencialesActuales();
     $user = $cred['username'];
     //$pass = escapeshellarg($cred['password']);
-    $pass=$cred['password'];
+    $pass = $cred['password'];
     $fecha = date("Ymd_His");
     $db = "abdarcproyecto1";
     $host = "localhost";
 
     $mysqldump = encontrarMysqldump();
- if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN') {
+    if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN') {
         $ruta = __DIR__ . "\\..\\DB\\backups\\respaldo_$fecha.sql";
 
-       // $comando =  "\"$mysqldump\" --single-transaction --skip-lock-tables --triggers --events --result-file=\"$ruta\" -u $user -p\"$pass\"  $db ";
-       $comando =  "\"$mysqldump\" --single-transaction --skip-lock-tables --result-file=\"$ruta\" -u $user -p\"$pass\"  $db ";
-       exec($comando . " 2>&1", $output, $resultado);
-        
-    }else {
+        // $comando =  "\"$mysqldump\" --single-transaction --skip-lock-tables --triggers --events --result-file=\"$ruta\" -u $user -p\"$pass\"  $db ";
+        $comando =  "\"$mysqldump\" --single-transaction --skip-lock-tables --result-file=\"$ruta\" -u $user -p\"$pass\"  $db ";
+        exec($comando . " 2>&1", $output, $resultado);
+    } else {
         $ruta = __DIR__ . "/../DB/backups/respaldo_$fecha.sql";
         $pass = escapeshellarg($pass);
-       // $comando = "$mysqldump --single-transaction --skip-lock-tables --triggers --events  -u $user -p$pass $db > $ruta";
-       $comando = "$mysqldump --single-transaction --skip-lock-tables --triggers --events  -u $user -p$pass $db > $ruta";
+        // $comando = "$mysqldump --single-transaction --skip-lock-tables --triggers --events  -u $user -p$pass $db > $ruta";
+        $comando = "$mysqldump --single-transaction --skip-lock-tables --triggers --events  -u $user -p$pass $db > $ruta";
         exec($comando, $output, $resultado);
     }
 
- if ($resultado === 0) {
+    if ($resultado === 0) {
 
-    $contenido =
-    file_get_contents($ruta);
+        $contenido =
+            file_get_contents($ruta);
 
-    // quitar DEFINER
-    $contenido = preg_replace(
-        '/DEFINER[ ]*=[ ]*`[^`]+`@`[^`]+`/i',
-        '',
-        $contenido
-    );
+        // quitar DEFINER
+        $contenido = preg_replace(
+            '/DEFINER[ ]*=[ ]*`[^`]+`@`[^`]+`/i',
+            '',
+            $contenido
+        );
 
-    // quitar SQL SECURITY DEFINER
-    $contenido = preg_replace(
-        '/SQL SECURITY DEFINER/i',
-        '',
-        $contenido
-    );
+        // quitar SQL SECURITY DEFINER
+        $contenido = preg_replace(
+            '/SQL SECURITY DEFINER/i',
+            '',
+            $contenido
+        );
 
-    // algunos formatos especiales
-    $contenido = preg_replace(
-        '/DEFINER=[^*]*\*/i',
-        '*',
-        $contenido
-    );
+        // algunos formatos especiales
+        $contenido = preg_replace(
+            '/DEFINER=[^*]*\*/i',
+            '*',
+            $contenido
+        );
 
-    file_put_contents(
-        $ruta,
-        $contenido
-    );
+        file_put_contents(
+            $ruta,
+            $contenido
+        );
 
-    echo "✅ Respaldo generado correctamente";
-} else {
-    echo implode("\n", $output);
+        echo "✅ Respaldo generado correctamente";
+    } else {
+        echo implode("\n", $output);
     }
-
 } catch (Exception $e) {
     echo "⛔ " . $e->getMessage();
 }
@@ -125,7 +123,7 @@ function encontrarMysqldump(): ?string
             "C:\\Program Files\\MySQL\\MySQL Server 8.0\\bin\\mysqldump.exe",
             "C:\\Program Files (x86)\\MySQL\\MySQL Server 8.0\\bin\\mysqldump.exe",
         ];
-    } 
+    }
     //Linux / servidor
     else {
         $output = trim(shell_exec("which mysqldump"));
@@ -146,4 +144,3 @@ function encontrarMysqldump(): ?string
 
     return null;
 }
-?>
